3ff24992cc6b9234943db4a639a180db53140494
Peter <github@peter.dev.br>
2023-11-06 20:39:24 -0300

    card matcher

--- Memorize/Model/MemoryGame.swift
+++ Memorize/Model/MemoryGame.swift
@@ -11,6 +11,19 @@ import Foundation
 struct MemoryGame<CardContent> where CardContent: Equatable {
   private(set) var cards: [Card]
   
+  var lastIndex: Int? {
+    get {
+      let indices = cards.indices.filter {!cards[$0].hidden}
+      return indices.count == 1 ? indices.first : nil
+    }
+    
+    set {
+      cards.indices.forEach { index in
+        cards[index].hidden = !(index == newValue)
+      }
+    }
+  }
+  
   init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
     cards = (0..<max(2, numberOfPairsOfCards)).map({ index in
       var cards: [Card] = []
@@ -25,15 +38,23 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     }).flatMap({ $0 }).shuffled()
   }
   
-  func index(after i: Int) -> Int {
-    cards.index(after: i)
-  }
-  
+ 
   mutating func choose(_ card: Card) {
     if let index = cards.firstIndex(of: card) {
-      var card = cards[index]
-      card.hidden.toggle()
-      cards[index] = card
+      if cards[index].hidden && !cards[index].matched {
+        
+        if let lastIndex {
+          if cards[lastIndex].content == cards[index].content {
+            cards[lastIndex].matched = true
+            cards[index].matched = true
+          }
+        } else {
+          lastIndex = index
+        }
+        
+        cards[index].hidden = false
+      }
+      
     }
   }
   
